1eed08017278f7f70c2980e5b5cfc97f0e425bf3
bha86 <65356637+bha86@users.noreply.github.com>
2020-05-20 22:02:02 -0500

    Update QuotRem.c

--- QuotRem.c
+++ QuotRem.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 int main() {
     int dividend = 2, divisor = 3, quotient = 4, remainder = 5;
-    //printf("Enter dividend: ");
-    //scanf("%d", &dividend);
-    //printf("Enter divisor: ");
-    //scanf("%d", &divisor);
 
-    // Computes quotient
     quotient = dividend / divisor;
 
-    // Computes remainder
+
     remainder = dividend % divisor;
 
     printf("Quotient = %d\n", quotient);
